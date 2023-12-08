1e56e902ae888722baff0ea6305ce3245397a060
sachx1 <sachx1281@gmail.com>
2023-12-08 10:39:22 -0500

    added footer buttons

--- src/components/containerOne.js
+++ src/components/containerOne.js
@@ -5,6 +5,9 @@ import ArchiveIcon from '@mui/icons-material/Archive';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
     
 const ContainerOne = () => {
     return (
@@ -18,7 +21,7 @@ const ContainerOne = () => {
                 </p>
             </div>
             <div className='containerBtn'>
-                <button>
+                <button className='buttons'>
                     <Stack direction="row" alignItems="center" gap={1}>
                         <InfoIcon />
                         <Typography variant="body1">About</Typography>
@@ -26,7 +29,7 @@ const ContainerOne = () => {
                 </button>
             </div>
             <div className='containerBtn'>
-                <button>
+                <button className='buttons'>
                     <Stack direction="row" alignItems="center" gap={1}>
                         <ArchiveIcon />
                         <Typography variant="body1">Experience</Typography>
@@ -34,12 +37,32 @@ const ContainerOne = () => {
                 </button>
             </div>
             <div className='containerBtn'>
-                <button>
+                <button className='buttons'>
                     <Stack direction="row" alignItems="center" gap={1}>
                         <PermContactCalendarIcon />
                         <Typography variant="body1">Contact</Typography>
                     </Stack>
                 </button>
+            </div>
+            <div className='containerBtn'>
+                <button className='buttons'>
+                    <Stack direction="row" alignItems="center" gap={1}>
+                        <PictureAsPdfIcon />
+                        <Typography variant="body1">Resume</Typography>
+                    </Stack>
+                </button>
+            </div>
+            <div className='containerFooter'>
+                <a href="https://github.com/sachx1" target="_blank" rel="noopener noreferrer">
+                    <button className='buttonsFooter'>
+                        <GitHubIcon />
+                    </button>
+                </a>
+                <a href="https://www.linkedin.com/in/sachin-khargie/" target="_blank" rel="noopener noreferrer">
+                    <button className='buttonsFooter'>
+                        <LinkedInIcon />
+                    </button>
+                </a>
             </div>
         </div>
     );
